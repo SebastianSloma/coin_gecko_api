9b44d6003834c1aea748bd6534b726cb7bbcedeb
Sebastian <sebastiansloma@gmail.com>
2023-04-16 11:38:42 +0200

    added public api

--- src/App.js
+++ src/App.js
@@ -3,9 +3,15 @@ import './App.css';
 import axios from 'axios';
 
 function App() {
+  const getData = async () =>{
+    const res = await axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en")
+    console.log(res.data);
+  }
+
   useEffect(()=>{
-    axios.get("")
-  })
+    getData()
+  }, [])
+
   return (
     <div className="App">
 <h1>Coin Market</h1>
