bcd775b18d011a9614a9629115c24e17863d1ecc
Sebastian <sebastiansloma@gmail.com>
2023-04-18 09:27:24 +0200

    show name of coins

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import axios from 'axios';
+import TableCoins from './components/TableCoins';
 
 function App() {
-  const getData = async () =>{
-    const res = await axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en")
-    console.log(res.data);
-  }
 
-  useEffect(()=>{
-    getData()
-  }, [])
+  const [coins,setCoins] = useState([])
 
-  return (
-    <div className="App">
-<h1>Coin Market</h1>
-    </div>
-  );
+	const getData = async () => {
+		const res = await axios.get(
+			'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en'
+      );
+      console.log(res.data);
+      setCoins(res.data)
+	};
+
+	useEffect(() => {
+		getData();
+	}, []);
+
+	return (
+		<div className='App'>
+			<h1>Coin Market</h1>
+      <TableCoins coins={coins}/>
+		</div>
+	);
 }
 
 export default App;
