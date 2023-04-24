0842a2fec3d2e4461e3589622f29ba58a1fed6b8
Sebastian <sebastiansloma@gmail.com>
2023-04-24 07:36:41 +0200

    added colors to 24h price

--- src/components/CoinRow.js
+++ src/components/CoinRow.js
@@ -3,17 +3,26 @@ import React from 'react';
 const CoinRow = ({ coin, index }) => {
 	return (
 		<tr>
+			<td>{index}</td>
 			<td>
-				<img src={coin.image} alt={coin.name} style={{width: '6%'}} className='img-fluid me-4'/>
+				<img
+					src={coin.image}
+					alt={coin.name}
+					style={{ width: '6%' }}
+					className='img-fluid me-4'
+				/>
 
-				<span>
-				{index}
-				</span>
-				
-				</td>
-			<td>{coin.name}</td>
+				<span>{coin.name}</span>
+				<span className='ms-3 text-muted text-uppercase'>{coin.symbol}</span>
+			</td>
 			<td>{coin.current_price}</td>
-			<td>{coin.price_change_percentage_24h}</td>
+			<td
+				className={
+					coin.price_change_percentage_24h > 0 ? 'text-success' : 'text-danger'
+				}
+			>
+				{coin.price_change_percentage_24h}
+			</td>
 			<td>{coin.total_volume}</td>
 		</tr>
 	);
