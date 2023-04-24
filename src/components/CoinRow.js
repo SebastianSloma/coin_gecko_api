a2d13fc3185f72b3297a29972b867aa16759602c
Sebastian <sebastiansloma@gmail.com>
2023-04-24 06:57:19 +0200

    created table with api

--- src/components/CoinRow.js
+++ src/components/CoinRow.js
@@ -3,8 +3,18 @@ import React from 'react';
 const CoinRow = ({ coin, index }) => {
 	return (
 		<tr>
-			<td>{index}</td>
+			<td>
+				<img src={coin.image} alt={coin.name} style={{width: '6%'}} className='img-fluid me-4'/>
+
+				<span>
+				{index}
+				</span>
+				
+				</td>
 			<td>{coin.name}</td>
+			<td>{coin.current_price}</td>
+			<td>{coin.price_change_percentage_24h}</td>
+			<td>{coin.total_volume}</td>
 		</tr>
 	);
 };
